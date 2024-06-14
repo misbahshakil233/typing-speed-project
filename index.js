4326dc34cf99026036171038c8753cc2ca5f6a11
Misbah Shakil <141862743+misbahshakil233@users.noreply.github.com>
2024-06-14 21:58:03 +0500

    Update index.js
    updated

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
+#! usr/bin/env node
 import inquirer from 'inquirer';
 import chalk from 'chalk';
-import readline from 'readline';
-import { promisify } from 'util';
-const sleep = promisify(setTimeout);
+import banner from "node-banner";
+
 class User {
-    name;
-    id;
-    password;
-    constructor(name, id, password) {
+    name: string;
+    id: string;
+    password: any;
+
+    constructor(name: string, id: string, password: any) {
         this.name = name;
         this.id = id;
         this.password = password;
     }
+
     static async SignUp() {
         const answers = await inquirer.prompt([
             {
                 name: "userName",
-                message: "Enter userName ",
+                message:  chalk.blue("Enter userName : "),
                 type: "input",
             },
             {
                 name: "userId",
-                message: "Enter an Email",
+                message:chalk.blue( "Enter an Email : "),
                 type: "input",
                 validate: function (value) {
                     if (value.trim() === "") {
                         return chalk.red("Please enter a non-empty string");
-                    }
-                    else {
+                    } else {
                         return true;
                     }
                 }
             },
             {
                 name: "password",
-                message: "Enter Password",
+                message: chalk.blue("Enter Password : "),
                 type: "input",
-                validate: function (input) {
+                validate: function (input: any) {
                     const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])/;
                     if (passwordRegex.test(input)) {
                         return true;
-                    }
-                    else {
+                    } else {
                         return chalk.blue('Password must contain at least one number and one special character.');
                     }
                 }
@@ -49,119 +49,154 @@ class User {
         ]);
         return new User(answers.userName, answers.userId, answers.password);
     }
-    static async Login(saved) {
+
+    static async Login(saved: User[]) {
         const answers = await inquirer.prompt([
             {
                 name: 'userid',
-                message: 'Enter Your User Email-ID :',
+                message: chalk.blue('Enter Your User Email-ID :'),
                 type: 'input'
             },
             {
                 name: 'password',
-                message: 'Enter Your Password :',
+                message: chalk.blue('Enter Your Password :'),
                 type: 'input',
                 mask: '*'
             }
         ]);
+
         const found = saved.find((user) => user.id === answers.userid && user.password === answers.password);
+
         if (found) {
             console.log(chalk.green(`Welcome back ${found.name}, Login successful`));
             return found;
-        }
-        else {
+        } else {
             console.log(chalk.red('Login failed: Invalid User ID or Password.'));
             return null;
         }
     }
+
     static async levelFun() {
         const answers = await inquirer.prompt([{
-                name: "level",
-                message: "What level do you want to select? Easy, Medium, or Difficult",
-                type: "list",
-                choices: ["Easy", "Medium", "Difficult"]
-            }]);
-        if (answers.level === "Easy") {
-            console.log(chalk.blue.bold("----WELCOME TO EASY LEVEL----"));
-            console.log(chalk.green("You selected the easy level. You must write the sentence within a minute."));
-            console.log(chalk.blue("Sentence to write: 'Hardworking is the key to success'"));
-            const userInput = await this.getUserInputWithinTime(60000); // 60 seconds
-            if (userInput) {
-                console.log(`You wrote: ${userInput}`);
-                const correctSentence = "Hardworking is the key to success";
-                if (userInput.trim() === correctSentence) {
-                    console.log(chalk.green("Your sentence is 100% correct"));
-                    const wordCount = correctSentence.split(' ').length;
-                    console.log(chalk.green(`Your word count is ${wordCount}`));
-                    console.log(chalk.green(`${this.name} selected the easy level and wrote the correct sentence with ${wordCount} words.`));
-                }
-                else {
-                    const userWords = userInput.split(' ');
-                    const correctWords = correctSentence.split(' ');
-                    let correctCount = 0;
-                    userWords.forEach((word, index) => {
-                        if (word === correctWords[index]) {
-                            correctCount++;
-                        }
-                    });
-                    const accuracy = (correctCount / correctWords.length) * 100;
-                    console.log(chalk.yellow(`You wrote ${userWords.length} words.`));
-                    console.log(chalk.yellow(`Your accuracy is ${accuracy.toFixed(2)}%.`));
-                    console.log(chalk.red("The sentence is incorrect."));
-                }
-            }
-            else {
-                console.log(chalk.red("Oops, you are very lazy, you didn't complete the task in time."));
-            }
+            name: "level",
+            message: chalk.hex('#FF00FF').bold('What level do you want to select? Easy, Medium, or Difficult'),
+            type: "list",
+            choices: ["Easy", "Medium", "Difficult"]
+        }]);
+    
+        let sentence = '';
+        let levelName = '';
+        let timeLimit = 0;
+    
+        switch (answers.level) {
+            case "Easy":
+                levelName = 'EASY';
+                sentence = "Hardworking is the key to success";
+                timeLimit = 120000; // 2 minutes
+                break;
+            case "Medium":
+                levelName = 'MEDIUM';
+                sentence = "Consistent effort over time is what leads to outstanding results";
+                timeLimit = 90000; // 1.5 minutes
+                break;
+            case "Difficult":
+                levelName = 'DIFFICULT';
+                sentence = "To achieve greatness, one must embrace challenges and persist through adversity";
+                timeLimit = 60000; // 1 minute
+                break;
         }
-    }
-    static async getUserInputWithinTime(timeLimit) {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        let userInput = '';
-        const inputPromise = new Promise((resolve) => {
-            rl.question('', (answer) => {
-                userInput = answer;
-                rl.close();
-                resolve(userInput);
-            });
-        });
-        const timeoutPromise = sleep(timeLimit).then(() => null);
-        const result = await Promise.race([inputPromise, timeoutPromise]);
-        if (!result) {
-            rl.close();
+    
+        console.log(chalk.blue.bold(`----WELCOME TO ${levelName} LEVEL----\n`));
+        console.log(chalk.green(`You selected the ${chalk.bold.gray(levelName)} level. You have ${chalk.bold.gray(timeLimit / 1000)} seconds to write the sentence.\n`));
+        console.log(chalk.blue(`Sentence to write: '${sentence}'\n`));
+    
+        const startTime = new Date();
+    
+        const levelAns = await inquirer.prompt([{
+            name: "level1",
+            message: "Please write the sentence here....",
+            type: "input"
+        }]);
+    
+        const endTime = new Date();
+        const timeTaken = (endTime.getTime() - startTime.getTime()) / 1000; //in seconds 
+    
+        if (timeTaken > timeLimit / 1000) {
+            console.log(chalk.red('Time limit exceeded!'));
         }
-        return result;
+    
+        // Calculate Words Per Minute (WPM)
+        const wordsTyped = levelAns.level1.trim().split(/\s+/).length;
+        const minutes = timeTaken / 60; //convert into minutes
+        const wpm = Math.round(wordsTyped / minutes);
+    
+        if (levelAns.level1.length === sentence.length && levelAns.level1.trim() === sentence) {
+            console.log("Your sentence is 100% correct\n");
+            console.log(chalk.greenBright(`Your word count is ${wordsTyped}`));
+            console.log(chalk.greenBright(`Your typing speed is approximately ${wpm} words per minute (WPM).\n`));
+            console.log(chalk.bold.green("\n\t ------RESULT--------\n\t "));
+            console.log(chalk.blue(`You selected the ${chalk.bold.gray(levelName)} level and corrected the sentence. Word count is ${chalk.bold.gray(wordsTyped)}`));
+        } else {
+            console.log("Your sentence is incorrect");
+        }
+        
+        console.log(chalk.yellow(`Time taken: ${timeTaken.toFixed(2)} seconds `));
+        console.log(chalk.yellow(`Time given: ${timeLimit / 1000} seconds`));
     }
+    
 }
+
 (async () => {
+    // Displaying banner
+    await banner('Typing Testing Speed', 'project 2');
+
     let Exit = false;
-    const users = [];
+    const users: User[] = [];
+    let loggedInUser: User | null = null;
+
     while (!Exit) {
-        const { action } = await inquirer.prompt([
-            {
-                name: 'action',
-                message: 'Would you like to Signup, Login, or Exit',
-                type: 'list',
-                choices: ['Signup', 'User Login', 'Exit']
-            }
-        ]);
-        switch (action) {
-            case "Signup":
-                const newUser = await User.SignUp();
-                users.push(newUser);
-                console.log(chalk.green("Signup successful"));
-                break;
-            case "User Login":
-                const loggedInUser = await User.Login(users);
-                if (loggedInUser) {
-                    await User.levelFun();
+        if (!loggedInUser) {
+            const { action } = await inquirer.prompt([
+                {
+                    name: 'action',
+                    message: chalk.hex('#FF00FF')('Would you like to Signup, Login, or Exit'),
+                    type: 'list',
+                    choices: ['Signup', 'User Login', 'Exit']
                 }
-                break;
-            case "Exit":
-                Exit = true;
-                break;
+            ]);
+
+            switch (action) {
+                case "Signup":
+                    const newUser = await User.SignUp();
+                    users.push(newUser);
+                    console.log(chalk.green("---------Signup successful---------"));
+                    break;
+                case "User Login":
+                    loggedInUser = await User.Login(users);
+                    console.log(chalk.green("--------login successful-------"));
+                    if (loggedInUser) {
+                        await User.levelFun();
+                    }
+                    break;
+                case "Exit":
+                    Exit = true;
+                    break;
+            }
+        } else {
+            await User.levelFun();
+            const { continueAction } = await inquirer.prompt([
+                {
+                    name: 'continueAction',
+                    message: chalk.hex('#FF00FF')('Would you like to continue or logout?'),
+                    type: 'list',
+                    choices: ['Continue', 'Logout']
+                }
+            ]);
+
+            if (continueAction === 'Logout') {
+                loggedInUser = null;
+            }
         }
     }
 })();
+
